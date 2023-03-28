b49e68d20a615e83f01bf516ee60fa74f7c4c74a
Simon Borkin <simon.borkin@transperfect.com>
2023-03-28 17:29:48 +0200

    Release 2.0.170

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@
 *Applanga iOS Documentation:* <https://www.applanga.com/docs-integration/ios> 
 ***
 
+### Version 2.0.170 (28 Mar 2023)
+#### Fixed
+- fix DefaultStringInterpolation result having format symbols instead of actual values
+
 ### Version 2.0.167 (3 Mar 2023)
 #### Added
 - Initial release of the Applanga SwiftUI SDK for iOS.
--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ApplangaSwiftUIWrapper"])
     ],
     dependencies: [
-        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.167")
+        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.170")
     ],
     targets: [
         .binaryTarget(name: "ApplangaSwiftUI",
