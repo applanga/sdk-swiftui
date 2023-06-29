38ff0c12244240a3c3d6f37917267ace50d72d4d
Jenkins <jenkins@MacBook-Pro-M1.fritz.box>
2023-06-29 18:03:25 +0200

    Release 2.0.173

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ApplangaSwiftUIWrapper"])
     ],
     dependencies: [
-        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.171")
+        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.173")
     ],
     targets: [
         .binaryTarget(name: "ApplangaSwiftUI",
