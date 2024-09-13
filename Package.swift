164673fa35c2e5abbfd6a724a38d85bf431afdd8
steffen <>
2024-09-13 17:18:05 +0200

    Release 2.0.196

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ApplangaSwiftUIWrapper"])
     ],
     dependencies: [
-        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.195")
+        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.196")
     ],
     targets: [
         .binaryTarget(name: "ApplangaSwiftUI",
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Applanga SwiftUI SDK for iOS Localization
 ***
-*Version:* 2.0.195
+*Version:* 2.0.196
 
 *Website:* <https://www.applanga.com> 
 
