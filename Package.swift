799562976b9ad46ee71e05f20840e1f7da098c6a
steffen-roemer <>
2026-05-07 11:35:59 +0200

    Release 2.0.228

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["ApplangaSwiftUIWrapper"])
     ],
     dependencies: [
-        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.226")
+        .package(name: "Applanga", url: "https://github.com/applanga/sdk-ios", from: "2.0.228")
     ],
     targets: [
         .binaryTarget(name: "ApplangaSwiftUI",
